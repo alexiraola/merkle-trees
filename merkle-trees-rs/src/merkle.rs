e841420466ad7f5849d3ba9d84fef5f3759e5c1c
Alex Iraola <alex.iraola@gmail.com>
2025-09-24 00:19:17 +0200

    Build tree

--- merkle-trees-rs/src/merkle.rs
+++ merkle-trees-rs/src/merkle.rs
@@ -7,25 +7,27 @@ fn hash_hex(data: &str) -> String {
     format!("{:x}", hash)
 }
 
+#[derive(Debug, Clone)]
 struct Node {
     hash: String,
+    left: Option<Box<Node>>,
+    right: Option<Box<Node>>,
 }
 
 impl Node {
     fn leaf(data: &str) -> Self {
-        // hash sha256 of data
-        let hash = hash_hex(data);
-        Self { hash }
+        Self {
+            hash: hash_hex(data),
+            left: None,
+            right: None,
+        }
     }
 
-    fn new(left: Node, right: Option<Node>) -> Self {
-        match right {
-            Some(node) => Self {
-                hash: hash_hex(&format!("{}{}", left.hash, node.hash)),
-            },
-            None => Self {
-                hash: hash_hex(&format!("{}{}", left.hash, left.hash)),
-            },
+    fn new(left: Node, right: Node) -> Self {
+        Self {
+            hash: hash_hex(&format!("{}{}", left.hash, right.hash)),
+            left: Some(Box::new(left.clone())),
+            right: Some(Box::new(right.clone())),
         }
     }
 }
@@ -41,12 +43,23 @@ impl MerkleTree {
     }
 
     fn build_tree(leaves: Vec<String>) -> Node {
-        if leaves.len() == 1 {
-            return Node::leaf(&leaves[0]);
+        let mut level: Vec<Node> = leaves.iter().map(|leaf| Node::leaf(leaf)).collect();
+
+        while level.len() > 1 {
+            let mut next_level: Vec<Node> = Vec::new();
+            for i in (0..level.len()).step_by(2) {
+                let left = level[i].clone();
+                let right = if i + 1 < level.len() {
+                    level[i + 1].clone()
+                } else {
+                    level[i].clone()
+                };
+
+                next_level.push(Node::new(left, right));
+            }
+            level = next_level;
         }
-        let left = Self::build_tree(leaves[0..leaves.len() / 2].to_vec());
-        let right = Self::build_tree(leaves[leaves.len() / 2..].to_vec());
-        Node::new(left, Some(right))
+        level.remove(0)
     }
 }
 
@@ -68,20 +81,10 @@ mod tests {
     fn test_creates_node_with_left_and_right() {
         let left = Node::leaf("Tx1");
         let right = Node::leaf("Tx2");
-        let node = Node::new(left, Some(right));
+        let node = Node::new(left, right);
         assert_eq!(
             node.hash,
             "0971909734e9c49e0f45caeb15a450d717de387a0a27df245e7e924bb7e62b0e".to_string()
-        );
-    }
-
-    #[test]
-    fn test_creates_node_with_left() {
-        let left = Node::leaf("Tx1");
-        let node = Node::new(left, None);
-        assert_eq!(
-            node.hash,
-            "5acc55c91078975c740a361887f5cd8fa7d75949accebcdd9d7cf4460814b82f".to_string()
         );
     }
 
@@ -97,6 +100,16 @@ mod tests {
         assert_eq!(
             tree.root.hash,
             "5b260dbcbff182d10cdbd21d8cb9e4446fe71820bb91c8dced8dcfd0e8a9c8ac".to_string()
+        );
+    }
+
+    #[test]
+    fn test_creates_merkle_tree_with_odd_number_of_leaves() {
+        let leaves = vec!["Tx1".to_string(), "Tx2".to_string(), "Tx3".to_string()];
+        let tree = MerkleTree::new(leaves);
+        assert_eq!(
+            tree.root.hash,
+            "d450c7864e6af68eab970295be53ea3d4e550b775079c366de34d21e15610add".to_string()
         );
     }
 }
