f9dc79ad4ea2315b25eeb177732065ece686d3ea
Alex Iraola <alex.iraola@gmail.com>
2025-09-23 22:39:20 +0200

    First tests

---
+++ merkle-trees-rs/src/merkle.rs
@@ -0,0 +1,102 @@
+use sha2::{Digest, Sha256};
+
+fn hash_hex(data: &str) -> String {
+    let mut hasher = Sha256::new();
+    hasher.update(data.as_bytes());
+    let hash = hasher.finalize();
+    format!("{:x}", hash)
+}
+
+struct Node {
+    hash: String,
+}
+
+impl Node {
+    fn leaf(data: &str) -> Self {
+        // hash sha256 of data
+        let hash = hash_hex(data);
+        Self { hash }
+    }
+
+    fn new(left: Node, right: Option<Node>) -> Self {
+        match right {
+            Some(node) => Self {
+                hash: hash_hex(&format!("{}{}", left.hash, node.hash)),
+            },
+            None => Self {
+                hash: hash_hex(&format!("{}{}", left.hash, left.hash)),
+            },
+        }
+    }
+}
+
+struct MerkleTree {
+    root: Node,
+}
+
+impl MerkleTree {
+    fn new(leaves: Vec<String>) -> Self {
+        let root = Self::build_tree(leaves);
+        Self { root }
+    }
+
+    fn build_tree(leaves: Vec<String>) -> Node {
+        if leaves.len() == 1 {
+            return Node::leaf(&leaves[0]);
+        }
+        let left = Self::build_tree(leaves[0..leaves.len() / 2].to_vec());
+        let right = Self::build_tree(leaves[leaves.len() / 2..].to_vec());
+        Node::new(left, Some(right))
+    }
+}
+
+// Test module
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_creates_leaf_with_data() {
+        let leaf = Node::leaf("Tx1");
+        assert_eq!(
+            leaf.hash,
+            "55f743d0d1b9bd86bbd96a46ba4272ddde19f09e3f6e47832e34bb2779a120b5".to_string()
+        );
+    }
+
+    #[test]
+    fn test_creates_node_with_left_and_right() {
+        let left = Node::leaf("Tx1");
+        let right = Node::leaf("Tx2");
+        let node = Node::new(left, Some(right));
+        assert_eq!(
+            node.hash,
+            "0971909734e9c49e0f45caeb15a450d717de387a0a27df245e7e924bb7e62b0e".to_string()
+        );
+    }
+
+    #[test]
+    fn test_creates_node_with_left() {
+        let left = Node::leaf("Tx1");
+        let node = Node::new(left, None);
+        assert_eq!(
+            node.hash,
+            "5acc55c91078975c740a361887f5cd8fa7d75949accebcdd9d7cf4460814b82f".to_string()
+        );
+    }
+
+    #[test]
+    fn test_creates_merkle_tree() {
+        let leaves = vec![
+            "Tx1".to_string(),
+            "Tx2".to_string(),
+            "Tx3".to_string(),
+            "Tx4".to_string(),
+        ];
+        let tree = MerkleTree::new(leaves);
+        assert_eq!(
+            tree.root.hash,
+            "5b260dbcbff182d10cdbd21d8cb9e4446fe71820bb91c8dced8dcfd0e8a9c8ac".to_string()
+        );
+    }
+}
